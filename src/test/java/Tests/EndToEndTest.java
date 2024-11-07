4a813f13eb77321491c66ad2f86df6a35f44b159
savithasarath <sabi.sadasivan@gmail.com>
2024-11-07 12:04:27 -0600

    webhooktest

--- src/test/java/Tests/EndToEndTest.java
+++ src/test/java/Tests/EndToEndTest.java
@@ -32,6 +32,7 @@ public class EndToEndTest extends BaseTest {
         OrderConfirmation orderConfirmation = new OrderConfirmation(driver);
         orderConfirmation.orderConfirmationMesageText.isDisplayed();
          orderNumber = orderConfirmation.getOrderNumber();
+         System.out.println("OrderNumber captured is: "+orderNumber);
     }
 
     @Test(dependsOnMethods = "submitOrderTest",groups={"returningUser","smoke"})
